c0a5313acf6e1bb2d5389dc98267a6aac8c06f11
milkshakeiii <milkshakeiii@gmail.com>
2015-05-28 17:36:08 -0400

    generic learning action

---
+++ ThinksyLearningAction.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class LearningAction
+{
+
+	private Senseix.Message.Problem.LearningAction learningActionProto;
+
+	public LearningAction(Senseix.Message.Problem.LearningAction newLearningActionProto)
+	{
+		learningActionProto = newLearningActionProto;
+	}
+
+	public Senseix.Message.Problem.LearningAction GetProto()
+	{
+		return new Senseix.Message.Problem.LearningAction ();
+	}
+
+}
